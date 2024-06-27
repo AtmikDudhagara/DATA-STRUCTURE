3f03cbbdf032a9cc9cb76ac3463b2fae7bfe905f
Atmik Dudhagara <atmikdudhagara1980@gmail.com>
2024-06-27 20:04:26 +0530

    update

--- ds/Anagram.java
+++ ds/Anagram.java
@@ -7,11 +7,11 @@ public class Anagram {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String arr[] = new String[n];
-        for(int i = 0 ; i<=n-1;i++){
+        for(int i = 0 ; i<=n;i++){
             arr[i]=sc.nextLine();
         }
-       // int index = rand.nextInt(0,n-1);
-       int index=2;
+        int index = rand.nextInt(0,n-1);
+       
         System.out.println(arr[index]);
         String c = arr[index];
         System.out.println("enter the word");
